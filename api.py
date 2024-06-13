ce52e62c86ba9f8ffe12e0f3d1f2626a7a520de4
hyanaka <166586642+hyanaka@users.noreply.github.com>
2024-06-13 20:00:04 +0900

    Add API for POST, PUT, DELETE

--- api.py
+++ api.py
@@ -5,16 +5,16 @@ from json import dumps
 api = Blueprint('api', __name__, url_prefix='/api')
 
 @api.route('/contents', methods = ['GET'])
-def get_contents():
+def getAll():
     data = Content.query.all()
     return dumps({'contents': [{'id': content.id, 'detail': content.detail} for content in data]})
 
 @api.route('/contents/<int:id>', methods=['GET'])
-def get_content(id):
+def get(id):
     content = Content.query.filter_by(id=id).first()
     return dumps({'id': content.id, 'detail': content.detail})
 
-@api.route('/add_content', methods=['POST'])
+@api.route('/contents', methods=['POST'])
 def add():
     json = request.get_json()
     entry = Content()
@@ -22,4 +22,22 @@ def add():
     db.session.add(entry)
     db.session.commit()
     db.session.close()
-    return f'Successfully added a content'
+    return 'Successfully added the content'
+
+@api.route('/contents/<int:id>', methods=['PUT'])
+def put(id):
+    json = request.get_json()
+    entry = Content.query.get(id)
+    entry.detail = json['detail']
+    db.session.merge(entry)
+    db.session.commit()
+    db.session.close()
+    return 'Successfully put the content'
+
+@api.route('/contents/<int:id>', methods=['DELETE'])
+def delete(id):
+    entry = Content.query.get(id)
+    db.session.delete(entry)
+    db.session.commit()
+    db.session.close()
+    return 'Successfully deleted the content'
